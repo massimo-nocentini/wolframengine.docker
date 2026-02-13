bd906b3080f5070f54bf6bff66bc2cb7d91fc8c0
Massimo Nocentini <massimo.nocentini@gmail.com>
2026-02-13 15:31:20 +0100

    Refactor install script to simplify WSTP file copying for amd64 architecture

--- install.sh
+++ install.sh
@@ -2,14 +2,12 @@
 echo "Building for platform: $BUILDPLATFORM"
 
 if [ $BUILDPLATFORM == "linux/amd64" ]; then
-  cp /usr/local/Wolfram/WolframEngine/${WE_VERSION}/SystemFiles/Links/WSTP/DeveloperKit/Linux-x86-64/CompilerAdditions/wstp.h /usr/local/include/
-  cp /usr/local/Wolfram/WolframEngine/${WE_VERSION}/SystemFiles/Links/WSTP/DeveloperKit/Linux-x86-64/CompilerAdditions/libWSTP64i4.so /usr/local/lib/
-else
-  apt-get update && apt-get upgrade -y && apt-get install -y build-essential ca-certificates
-  # yes | apt-get install -y ./wolfram-engine_14.3.0+202510021899_arm64.deb
-  cp /opt/Wolfram/WolframEngine/14.3/SystemFiles/Links/WSTP/DeveloperKit/Linux-ARM64/CompilerAdditions/wstp.h /usr/local/include/
-  cp /opt/Wolfram/WolframEngine/14.3/SystemFiles/Links/WSTP/DeveloperKit/Linux-ARM64/CompilerAdditions/libWSTP64i4.so /usr/local/lib/amd64/
+  cp -r /usr/local/Wolfram/WolframEngine/${WE_VERSION}/SystemFiles/Links/WSTP/DeveloperKit/Linux-x86-64/CompilerAdditions/ $HOME
+  # cp /usr/local/Wolfram/WolframEngine/${WE_VERSION}/SystemFiles/Links/WSTP/DeveloperKit/Linux-x86-64/CompilerAdditions/libWSTP64i4.so /usr/local/lib/amd64/
 fi
-
-rm -rf /usr/local/include/amd64 # Remove the directory if it exists, since we're copying files directly to /usr/local/include/
-
+# else
+#   apt-get update && apt-get upgrade -y && apt-get install -y build-essential ca-certificates
+#   # yes | apt-get install -y ./wolfram-engine_14.3.0+202510021899_arm64.deb
+#   cp /opt/Wolfram/WolframEngine/14.3/SystemFiles/Links/WSTP/DeveloperKit/Linux-ARM64/CompilerAdditions/wstp.h /usr/local/include/
+#   cp /opt/Wolfram/WolframEngine/14.3/SystemFiles/Links/WSTP/DeveloperKit/Linux-ARM64/CompilerAdditions/libWSTP64i4.so /usr/local/lib/
+# fi
